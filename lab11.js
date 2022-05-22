b42f1fe3bb5b7f899b246492d6d6e257f8877dcd
anastasialoz <62766554+anastasialoz@users.noreply.github.com>
2022-05-22 23:07:17 +0300

    Add files via upload

---
+++ lab11.js
@@ -0,0 +1,25 @@
+function f1()
+    {var a1, P1,S1;
+        a1=31;
+        P1=4*a1;
+        S1=a1*a1;
+    document.getElementById('task1').innerHTML='P='+P1+'<br>'+'S='+S1;
+    }
+
+    function f2()
+    {var x, y,max;
+        x=11.56;
+        y=10;
+        if(x>y)
+            {max=x;}
+        else
+            {max=y;}
+    document.getElementById('t2').innerHTML="max(x,y)="+max;}
+  function f2()
+    {var x, y,max;
+        x=250.56;
+        y=10;
+        max=x;
+        if(max<y)
+            {max=y;}
+        document.getElementById('t2').innerHTML="max(x,y)="+max;}
